533d8775588d742dff858509b74430c4af4f5950
Venkata Madhu <vvmadhu@yahoo.com>
2021-11-24 21:08:17 +0530

    AVRO Serialization and Deserialization

---
+++ AvroDeserializer.java
@@ -0,0 +1,56 @@
+package com.hcl.dna.ads.framework.process.serializer;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import javax.xml.bind.DatatypeConverter;
+
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.Decoder;
+import org.apache.avro.io.DecoderFactory;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.serialization.Deserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AvroDeserializer<T extends SpecificRecordBase> implements Deserializer<T> {
+
+    protected final Class<T> targetType;
+
+    public AvroDeserializer(Class<T> targetType) {
+        this.targetType = targetType;
+    }
+
+    @Override
+    public void close() {
+        // No-op
+    }
+
+    @Override
+    public void configure(Map<String, ?> arg0, boolean arg1) {
+        // No-op
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public T deserialize(String topic, byte[] data) {
+        try {
+            T result = null;
+
+            if (data != null) {
+                DatumReader<GenericRecord> datumReader =
+                        new SpecificDatumReader<>(targetType.newInstance().getSchema());
+                Decoder decoder = DecoderFactory.get().binaryDecoder(data, null);
+
+                result = (T) datumReader.read(null, decoder);
+            }
+            return result;
+        } catch (Exception ex) {
+            throw new SerializationException(
+                    "Can't deserialize data '" + Arrays.toString(data) + "' from topic '" + topic + "'", ex);
+        }
+    }
+}
